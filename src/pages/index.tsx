6e2e3811574d5d8749591b6d421848f4de38949b
suvarnakale <suvarna.kale@tekditechnologies.com>
2024-05-06 17:03:20 +0530

    Ticket #PS-318 feat: Login using password and username

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
-// import Head from 'next/head';
-// import Image from 'next/image';
-// import { Inter } from 'next/font/google';
-// import styles from '@/styles/Home.module.css';
-import LoginPage from './LoginPage';
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
 import i18n from '../i18n';
 import { I18nextProvider } from 'react-i18next';
+import Login from './Login';
 
 // const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  
+  console.log('hi');
   return (
     <>
       <I18nextProvider i18n={i18n}>
-      <LoginPage />
+        <Login />
       </I18nextProvider>
     </>
   );
