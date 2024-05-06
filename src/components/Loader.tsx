3dc4b0926b40c2ab763d80ed0461da27d3cec27a
Rushikesh-Sonawane99 <rushikesh.sonawane@tekditechnologies.com>
2024-05-06 16:21:50 +0530

    Issue #000 feat: added loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,31 @@
+import { Backdrop, CircularProgress, Typography } from '@mui/material';
+import React from 'react';
+
+const Loader: React.FC<{ showBackdrop: boolean; loadingText: string }> = ({
+  showBackdrop,
+  loadingText = ''
+}) => {
+  const Spinner = () => {
+    return (
+      <>
+        <CircularProgress color="inherit" />
+        <br />
+        <Typography variant="h2">{loadingText}...</Typography>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1, flexDirection: 'column' }}
+        open={showBackdrop}
+      >
+        <Spinner />
+      </Backdrop>
+      {!open && <Spinner />}
+    </>
+  );
+};
+
+export default Loader;
