001808b5a56ea226def130085526476006b4fed0
TTPL-RT-52 <shreyas1434shinde@gmail.com>
2024-05-17 11:21:55 +0530

    Task PS394 style: Styling optimization to Month view Calender

--- src/components/MenuDrawer.tsx
+++ src/components/MenuDrawer.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import React, { useEffect } from 'react';
-
 import { Button, FormControl, IconButton, MenuItem } from '@mui/material';
+import React, { useEffect } from 'react';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 import Box from '@mui/material/Box';
@@ -11,9 +10,10 @@ import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
 import Drawer from '@mui/material/Drawer';
 import LocalLibraryOutlinedIcon from '@mui/icons-material/LocalLibraryOutlined';
 import config from '../../config.json';
+import { useRouter } from 'next/router';
 import { useTheme } from '@mui/material/styles';
 import { useTranslation } from 'next-i18next';
-import { useRouter } from 'next/router';
+
 interface DrawerProps {
   toggleDrawer: (open: boolean) => () => void;
   open: boolean;
@@ -50,7 +50,7 @@ const MenuDrawer: React.FC<DrawerProps> = ({
   return (
     <div>
       <Drawer open={open} onClose={toggleDrawer(false)}>
-        <Box sx={{ padding: '16px 16px 12px 16px', width: '300px' }}>
+        <Box sx={{ padding: '16px 16px 12px 16px', width: '350px' }}>
           <Box
             sx={{
               display: 'flex',
