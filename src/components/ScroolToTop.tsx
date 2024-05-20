c12038a72d3eeb6445942e07fbb65481fb048a7d
TTPL-RT-52 <shreyas1434shinde@gmail.com>
2024-05-20 15:49:21 +0530

    Issue PS-328 fix: Custom button to scroll to the top/bottom of the page

---
+++ src/components/ScroolToTop.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from 'react';
+
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { Box } from '@mui/material';
+
+const UpDownButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isAtBottom, setIsAtBottom] = useState(false);
+
+  const trackScroll = () => {
+    const scrolled = window.pageYOffset;
+    const viewportHeight = document.documentElement.clientHeight;
+    const totalHeight = document.documentElement.scrollHeight;
+
+    if (scrolled > 100) {
+      // Show button quickly after scrolling starts
+      setIsVisible(true);
+      setIsAtBottom(false);
+    } else if (scrolled === 0) {
+      setIsAtBottom(true);
+      setIsVisible(true);
+    } else if (scrolled + viewportHeight >= totalHeight - 10) {
+      // Adjust for small discrepancies with a 10px buffer
+      setIsAtBottom(true);
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const backToTop = () => {
+    const scrollStep = -window.pageYOffset / (500 / 15);
+    const scrollInterval = setInterval(() => {
+      if (window.pageYOffset !== 0) {
+        window.scrollBy(0, scrollStep);
+      } else {
+        clearInterval(scrollInterval);
+      }
+    }, 15);
+  };
+
+  const scrollToBottom = () => {
+    const targetPosition =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight -
+      2000;
+    const scrollStep = (targetPosition - window.pageYOffset) / (500 / 15);
+    const scrollInterval = setInterval(() => {
+      if (window.pageYOffset < targetPosition) {
+        window.scrollBy(0, scrollStep);
+      } else {
+        clearInterval(scrollInterval);
+      }
+    }, 15);
+  };
+
+  const handleButtonClick = () => {
+    if (isAtBottom) {
+      scrollToBottom();
+    } else {
+      backToTop();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', trackScroll);
+    return () => {
+      window.removeEventListener('scroll', trackScroll);
+    };
+  }, []);
+
+  return (
+    <div>
+      {isVisible && (
+        <Box
+          className={`up_down_btn ${isVisible ? 'up_down_btn-show' : ''}`}
+          onClick={handleButtonClick}
+          title={isAtBottom ? 'Вниз' : 'Наверх'}
+        >
+          <span>{isAtBottom ? '' : <ArrowUpwardIcon />}</span>
+          <span className="w-98">
+            {isAtBottom ? 'Learners' : 'Back to Top'}
+          </span>
+          <span>{isAtBottom ? <ArrowDownwardIcon /> : ''}</span>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+export default UpDownButton;
