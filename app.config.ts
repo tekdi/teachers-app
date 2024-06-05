4d4536b229d4d85404125bf8a9d937bc396273c1
suvarnakale <suvarna.kale@tekditechnologies.com>
2024-06-05 10:26:21 +0530

    Issue #000 bug: PR updated

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,4 @@
 export const lowLearnerAttendanceLimit: number = 33;
 export const dashboardDaysLimit: number = 30;
+export const startDateLimit: number = 29;
 export const modifyAttendanceLimit: number = 6;
