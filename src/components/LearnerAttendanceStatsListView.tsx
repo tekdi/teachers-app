832e1438537a5ef0a537005c93cee33810179570
TTPL-RT-52 <shreyas1434shinde@gmail.com>
2024-06-06 14:13:16 +0530

    Issue #PS-657 fix:Tracker Bug fixes

--- src/components/LearnerAttendanceStatsListView.tsx
+++ src/components/LearnerAttendanceStatsListView.tsx
@@ -113,7 +113,7 @@ const StudentsStatsList: React.FC<StudentsStatsListProps> = ({
       )}
       <Stack>
         <Box
-          height="60px"
+          // height="60px"
           borderTop={`1px solid  ${theme.palette.warning['A100']}`}
           margin="0px"
           alignItems={'center'}
@@ -127,7 +127,7 @@ const StudentsStatsList: React.FC<StudentsStatsListProps> = ({
             p={2}
           >
             <Grid item xs={6} textAlign={'left'}>
-              <Link href={''}>
+              <Link className="word-break" href={''}>
                 <Typography
                   onClick={() => handleOpenModalLearner(userId!)}
                   sx={{
