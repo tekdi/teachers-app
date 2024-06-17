667c1a113d360f8e47c398b39a2efc0a3a092a2c
Rushikesh-Sonawane99 <rushikesh.sonawane@tekditechnologies.com>
2024-06-17 18:33:42 +0530

    Issue #PS-793 chore: Added dropout reasons and related transaltions

--- app.config.ts
+++ app.config.ts
@@ -3,3 +3,16 @@ export const avgLearnerAttendanceLimit: number = 66;
 export const dashboardDaysLimit: number = 30;
 export const modifyAttendanceLimit: number = 6;
 export const toastAutoHideDuration: number = 5000; // 5 seconds
+
+export const dropoutReasons = [
+    { label: 'UNABLE_TO_COPE_WITH_STUDIES', value: 'unable_to_cope_with_studies' },
+    { label: 'FAMILY_RESPONSIBILITIES', value: 'family_responsibilities' },
+    { label: 'NEED_TO_GO_TO_WORK_OWN_WORK', value: 'need_to_go_to_work_own_work' },
+    { label: 'MARRIAGE', value: 'marriage' },
+    { label: 'ILLNESS', value: 'illness' },
+    { label: 'MIGRATION', value: 'migration' },
+    { label: 'PREGNANCY', value: 'pregnancy' },
+    { label: 'DOCUMENT_ISSUE', value: 'document_issue' },
+    { label: 'DISTANCE_ISSUE', value: 'distance_issue' },
+    { label: 'SCHOOL_ADMISSION', value: 'school_admission' }
+  ];
