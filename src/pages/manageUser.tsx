01aa199780e12018f9685e5eb8636b4174323577
TTPL-RT-52 <shreyas1434shinde@gmail.com>
2024-06-18 19:18:01 +0530

    Issue #PS-798 fix: Implementation of UI component as per figma to assign facilitator to village/centre at district level

---
+++ src/pages/manageUser.tsx
@@ -0,0 +1,202 @@
+import { FormControl, Grid, MenuItem, Select, TextField } from '@mui/material';
+
+import Box from '@mui/material/Box';
+import Header from '@/components/Header';
+import InputAdornment from '@mui/material/InputAdornment';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useRouter } from 'next/router';
+import { useTheme } from '@mui/material/styles';
+import { useTranslation } from 'next-i18next';
+
+const manageUser = () => {
+  const { t } = useTranslation();
+  const theme = useTheme<any>();
+  const router = useRouter();
+
+  const [value, setValue] = React.useState('one');
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+  return (
+    <>
+      <Header />
+      <Box sx={{ padding: '0 18px' }}>
+        <Box
+          textAlign={'left'}
+          fontSize={'22px'}
+          p={'18px 0'}
+          color={theme?.palette?.warning['300']}
+        >
+          {t('COMMON.MANAGE_USERS')}
+        </Box>
+      </Box>
+      <Box sx={{ width: '100%' }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor="inherit" // Use "inherit" to apply custom color
+          aria-label="secondary tabs example"
+          sx={{
+            fontSize: '14px',
+            borderBottom: '1px solid #EBE1D4',
+
+            '& .MuiTab-root': {
+              color: '#4D4639',
+              padding: '0 20px',
+            },
+            '& .Mui-selected': {
+              color: '#4D4639',
+            },
+            '& .MuiTabs-indicator': {
+              display: 'flex',
+              justifyContent: 'center',
+              backgroundColor: theme.palette.primary.main,
+              borderRadius: '100px',
+              height: '3px',
+            },
+            '& .MuiTabs-scroller': {
+              overflowX: 'unset !important',
+            },
+          }}
+        >
+          {/* <Tab value="one" label="Center Sessions" />
+          <Tab value="two" label="Online Recordings" /> */}
+          <Tab value="one" label={t('COMMON.FACILITATORS')} />
+        </Tabs>
+      </Box>
+      <Box>
+        {value === 'one' && (
+          <>
+            <Grid
+              px={'18px'}
+              spacing={2}
+              mt={1}
+              sx={{ display: 'flex', alignItems: 'center' }}
+              container
+            >
+              <Grid item xs={8}>
+                <Box>
+                  <TextField
+                    className="input_search"
+                    placeholder="Search Facilitators.."
+                    color="secondary"
+                    focused
+                    sx={{
+                      borderRadius: '100px',
+                      height: '40px',
+                      // width: '225px',
+                    }}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <SearchIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </Box>
+              </Grid>
+              <Grid item xs={4} marginTop={'8px'}>
+                <Box>
+                  <FormControl className="drawer-select" sx={{ width: '100%' }}>
+                    <Select
+                      className="SelectLanguages"
+                      displayEmpty
+                      style={{
+                        borderRadius: '0.5rem',
+                        color: theme.palette.warning['200'],
+                        width: '100%',
+                        marginBottom: '0rem',
+                      }}
+                    >
+                      <MenuItem className="text-4D fs-14 fw-500">
+                        {t('COMMON.FILTERS')}
+                      </MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+              </Grid>
+            </Grid>
+
+            <Box>
+              <Box
+                px={'18px'}
+                mt={3}
+                sx={{ borderBottom: '1px solid #D0C5B4' }}
+              >
+                <Box
+                  sx={{
+                    display: 'flex',
+                    gap: '20px',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    paddingBottom: '15px',
+                  }}
+                >
+                  <Box
+                    sx={{ display: 'flex', gap: '15px', alignItems: 'center' }}
+                  >
+                    <Box>
+                      <Box
+                        sx={{
+                          fontSize: '16px',
+                          color: theme.palette.warning['300'],
+                        }}
+                      >
+                        Aditi Patel
+                      </Box>
+                      <Box
+                        sx={{
+                          fontSize: '12px',
+                          color: theme.palette.warning['300'],
+                          fontWeight: '500',
+                          marginTop: '2px',
+                        }}
+                      >
+                        Nagpur, Bhivapur
+                      </Box>
+                      <Box
+                        sx={{
+                          fontSize: '12px',
+                          color: theme.palette.warning['300'],
+                          fontWeight: '500',
+                          padding: '4px',
+                        }}
+                        className="color-FF"
+                      >
+                        Bhiwapur, Jabarbodi, Kargaon,
+                        <span style={{ color: theme.palette.warning['400'] }}>
+                          and 3 more
+                        </span>
+                      </Box>
+                    </Box>
+                  </Box>
+                  <MoreVertIcon
+                    sx={{
+                      fontSize: '24px',
+                      color: theme.palette.warning['300'],
+                    }}
+                  />
+                </Box>
+              </Box>
+            </Box>
+          </>
+        )}
+      </Box>
+    </>
+  );
+};
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+      // Will be passed to the page component as props
+    },
+  };
+}
+export default manageUser;
