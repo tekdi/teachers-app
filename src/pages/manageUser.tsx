2cfd64939f3bb9760cdcbec44734841d3a77cd57
TTPL-RT-52 <shreyas1434shinde@gmail.com>
2024-06-19 14:51:40 +0530

    Issue #PS-798 fix: Implementation of UI component as per figma to assign facilitator to village/centre at district level

--- src/pages/manageUser.tsx
+++ src/pages/manageUser.tsx
@@ -170,7 +170,6 @@ const manageUser = () => {
                         <span
                           style={{
                             color: theme.palette.warning.contrastText,
-
                             fontWeight: '500',
                           }}
                         >
