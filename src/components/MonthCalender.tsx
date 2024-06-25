3b68d48913f6160a3f190c6b76205ba462a3092e
Rushikesh-Sonawane99 <rushikesh.sonawane@tekditechnologies.com>
2024-06-25 12:12:37 +0530

    Issue #PS-626 chore: Fixed selected date range retention issue

--- src/components/MonthCalender.tsx
+++ src/components/MonthCalender.tsx
@@ -18,6 +18,7 @@ interface CalendarWithAttendanceProps {
   onChange: (date: Date) => void;
   onDateChange: (date: Date | Date[] | null) => void;
   selectionType?: 'single' | 'range';
+selectedRangeRetention?:Date | null | undefined | [Date | null, Date | null];
 }
 
 type AttendanceData = {
@@ -39,6 +40,7 @@ const MonthCalender: React.FC<CalendarWithAttendanceProps> = ({
   onChange,
   onDateChange,
   selectionType,
+  selectedRangeRetention
 }) => {
   const [date, setDate] = useState<
     Date | null | undefined | [Date | null, Date | null]
@@ -200,6 +202,17 @@ const MonthCalender: React.FC<CalendarWithAttendanceProps> = ({
     }
   }
 
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const retentionDate = localStorage.getItem('selectedRangeArray');
+      if (retentionDate) {
+          let retention = JSON.parse(retentionDate);
+          if (retention) {
+            handleDateChange(retention);
+          }
+        }
+    }
+  }, []);
   function tileClassName({ date, view }: { date: Date; view: string }) {
     if (view !== 'month') return null;
     const classes = [
@@ -257,6 +270,11 @@ const MonthCalender: React.FC<CalendarWithAttendanceProps> = ({
     };
     setDate(newDate);
     if (newDate !== undefined) {
+      localStorage.setItem('selectedRangeArray', JSON.stringify(newDate));
+    } else {
+      console.error('newDate is undefined');
+    }
+        if (newDate !== undefined) {
       let datesToSet: [Date | null, Date | null];
       if (Array.isArray(newDate)) {
         datesToSet = [newDate[0] || null, newDate[1] || null];
