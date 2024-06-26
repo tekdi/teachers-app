46c621469e9ae1b76c9600e0ffdb8293d61fa0a4
Arif <arifmajeed007.as@gmail.com>
2024-06-26 13:33:01 +0530

    Issue #PS-919 feat: [FRONTEND] Implementation of caching

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -1,14 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { getUserDetails } from "./ProfileService";
 import { cohortList } from "./CohortServices";
+import { refetchInterval, gcTime } from "@/utils/app.constant";
 
 export function useProfileInfo(userId: string | string[], fieldValue: boolean) {
   return useQuery({
     
     queryKey: ['profile', userId], 
     queryFn: () => getUserDetails(userId, fieldValue),
-    refetchInterval: 5 * 60 * 1000, 
-    gcTime: 10 * 60 * 1000
+    refetchInterval: refetchInterval, 
+    gcTime: gcTime
   });
 }
 
@@ -16,8 +17,8 @@ export function useCohortList( limit: any, page: any, filters: any) {
     return useQuery({
       queryKey: ['cohort'], 
       queryFn: () => cohortList({limit , page, filters}),
-      refetchInterval: 5 * 60 * 1000, 
-      gcTime: 10 * 60 * 1000
+      refetchInterval: refetchInterval, 
+      gcTime: gcTime
     });
   }
   
