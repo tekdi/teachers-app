8056c45c29317b2d8cf932d1bcb755efbaf8b7c7
suvarnakale <suvarna.kale@tekditechnologies.com>
2024-07-01 16:18:59 +0530

    Issue #000 chore: build issue resolved

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -253,7 +253,7 @@ const Header: React.FC = () => {
           primary: t('COMMON.LOGOUT'),
           secondary: t('COMMON.CANCEL'),
         }}
-        handleCloseModel={handleCloseModel}
+        handleCloseModal={handleCloseModel}
         modalOpen={modalOpen}
       />
 
