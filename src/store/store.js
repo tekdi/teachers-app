d7e3cbf7964df54b23389346316b006a830829bc
Rushikesh-Sonawane99 <rushikesh.sonawane@tekditechnologies.com>
2024-07-04 22:35:18 +0530

    Issue #PS-1154 chore: Added configuration for implementing role based access

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,9 @@ import { persist } from 'zustand/middleware';
 
 const useStore = create(persist((set) => ({
     value: '',
+    role: '',
     setValue: (newValue) => set((state) => ({ value: newValue })),
+    setUserRole: (newRole) => set((state) => ({ userRole: newRole })),
 }), {
 name: 'teacherApp',
 getStorage: () => localStorage,
