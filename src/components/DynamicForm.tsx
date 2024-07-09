2b4eb01a4c8782c1cbe246ef0c006bb98fc34f76
suvarnakale <suvarna.kale@tekditechnologies.com>
2024-07-09 15:05:31 +0530

    Issue #PS-1241 chore: solve build issue

--- src/components/DynamicForm.tsx
+++ src/components/DynamicForm.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import Form, { IChangeEvent } from '@rjsf/core';
+import { IChangeEvent } from '@rjsf/core';
 import ISubmitEvent from '@rjsf/core';
 import validator from '@rjsf/validator-ajv8';
-import { Theme as MaterialUITheme } from '@rjsf/material-ui';
+import { Theme as MaterialUITheme } from '@rjsf/mui';
 import { withTheme } from '@rjsf/core';
 
 const FormWithMaterialUI = withTheme(MaterialUITheme);
 
 interface DynamicFormProps {
-  schema: any;
-  uiSchema: any;
-  formData?: any;
+  schema: object;
+  uiSchema: object;
+  formData?: object;
   onSubmit: (data: ISubmitEvent<any>, event: React.FormEvent<any>) => void;
   onChange: (event: IChangeEvent<any>) => void;
   onError: (errors: any) => void;
