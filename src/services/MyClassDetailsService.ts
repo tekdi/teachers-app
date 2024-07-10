fc6d713a29e6c675ec0f99ba021ffe274bceff74
Arif <arifmajeed007.as@gmail.com>
2024-07-10 16:09:18 +0530

    Issue #PS-1098 feat: Show Facilitators list into the selected center

--- src/services/MyClassDetailsService.ts
+++ src/services/MyClassDetailsService.ts
@@ -2,6 +2,7 @@ import { Role, Status } from '@/utils/app.constant';
 import {
   cohortMemberList,
   updateCohortMemberStatusParams,
+  userList,
 } from '../utils/Interfaces';
 import { post, put } from './RestClient';
 
@@ -16,6 +17,28 @@ const fetchCohortMemberList = async ({
       limit,
       page,
       filters,
+    });
+    console.log('data', response?.data);
+    return response?.data;
+  } catch (error) {
+    console.error('error in cohort member list API ', error);
+    // throw error;
+  }
+};
+
+export const getMyUserList = async ({
+  limit,
+  page,
+  filters,
+  fields
+}: userList): Promise<any> => {
+  const apiUrl: string = `${process.env.NEXT_PUBLIC_BASE_URL}/list`;
+  try {
+    const response = await post(apiUrl, {
+      limit,
+      page,
+      filters,
+      fields
     });
     console.log('data', response?.data);
     return response?.data;
