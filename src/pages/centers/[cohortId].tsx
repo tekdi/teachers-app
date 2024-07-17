5dbca79ff0e5a58e5aa39328e00b7a800d9892c8
TTPL-RT-52 <shreyas1434shinde@gmail.com>
2024-07-17 16:44:11 +0530

    Issue #PS-1333 fix: Select Topic Sub-topic modal UI Implementation

--- src/pages/centers/[cohortId].tsx
+++ src/pages/centers/[cohortId].tsx
@@ -26,6 +26,7 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import PlannedSession from '@/components/PlannedSession';
 import RenameCenterModal from '@/components/center/RenameCenterModal';
 import Schedule from './../../components/Schedule';
+import SelectTopic from '@/components/SelectTopic';
 import { Session } from '../../utils/Interfaces';
 import SessionCard from '@/components/SessionCard';
 import SessionCardFooter from '@/components/SessionCardFooter';
@@ -285,7 +286,7 @@ const TeachingCenterDetails = () => {
             title={'Schedule'}
             primary={'Next'}
           >
-            <TopicDetails />
+            <SelectTopic />
           </CenterSessionModal>
           <Box mt={3} px={'18px'}>
             <Box
