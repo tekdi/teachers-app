dfe4ae5ad7339c628aec78d331cb6a23b394e8a9
suvarnakale <suvarna.kale@tekditechnologies.com>
2024-08-08 14:51:02 +0530

    Issue #PS-1662 chore: comments resolved

--- src/styles/modalStyles.tsx
+++ src/styles/modalStyles.tsx
@@ -1,5 +1,5 @@
-export const modalStyles = (theme: any) => ({
-  width: '85%',
+export const modalStyles = (theme: any, width?: string) => ({
+  width: width ? '65%' : '85%',
   position: 'absolute',
   top: '50%',
   left: '50%',
@@ -8,20 +8,6 @@ export const modalStyles = (theme: any) => ({
   borderRadius: '8px',
   zIndex: '9999',
   boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
-  '@media (min-width: 600px)': {
-    width: '450px',
-  },
-});
-
-export const SendCredstyle = (theme: any) => ({
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '65%',
-  boxShadow: 24,
-  bgcolor: '#fff',
-  borderRadius: '16px',
   '@media (min-width: 600px)': {
     width: '450px',
   },
