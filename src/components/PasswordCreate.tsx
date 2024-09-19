8c62e6477acffe57f559f07188b3aa50883238e7
ttpl-rt-217 <shreyas_s@techjoomla.com>
2024-09-19 17:58:30 +0530

    Task #1987: dashboard UI enhancement and theme configuration

--- src/components/PasswordCreate.tsx
+++ src/components/PasswordCreate.tsx
@@ -77,7 +77,7 @@ const PasswordCreate: React.FC<PasswordCreateProps> = ({
               color: passwordError ? theme.palette.warning['A200'] : 'inherit',
             },
           }}
-          helperText={passwordError && t('LOGIN_PAGE.PASSWORD_NEED')}
+          helperText={passwordError && t('LOGIN_PAGE.YOUR_PASSWORD_NEED')}
           label={t('LOGIN_PAGE.PASSWORD')}
           fullWidth
           sx={{
