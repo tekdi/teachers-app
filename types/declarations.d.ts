86c95a4b8a0738eb6d4bd6c9036fb2481d7b094c
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2024-10-11 11:49:24 +0530

    Issue feat:2168 Integrate questionary component

---
+++ types/declarations.d.ts
@@ -0,0 +1,4 @@
+declare module 'questionnaire-webcomponent/questionnaire-player-webcomponent.js' {
+    const content: any; // You can replace `any` with a more specific type if you have one
+    export default content;
+  }
