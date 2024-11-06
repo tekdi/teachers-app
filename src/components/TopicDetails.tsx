59ed53442c717dc3e87c5e8eb5f41af8f9440eaf
suvarnakale <suvarna.kale@tekditechnologies.com>
2024-11-06 12:59:14 +0530

    Issue #PS-2281 fixes: fixes in player integration

--- src/components/TopicDetails.tsx
+++ src/components/TopicDetails.tsx
@@ -59,6 +59,7 @@ const TopicDetails: React.FC<TopicDetailsProps> = ({
   };
 
   const handlePlayers = (identifier: string) => {
+    sessionStorage.setItem('previousPage', window.location.href);
     router.push(`/play/content/${identifier}`);
   };
 
