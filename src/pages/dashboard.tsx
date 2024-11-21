87f97d16b42123b2f3516ce68d291641b5f88ca2
ttpl-rt-217 <shreyas_s@techjoomla.com>
2024-11-21 13:29:48 +0530

    Issue #PS-2322 fix: In app notification device token was only genrating on dashboard fix issue

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -81,7 +81,7 @@ import { fetchAttendanceDetails } from '@/components/AttendanceDetails';
 
 import dynamic from 'next/dynamic';
 import { isEliminatedFromBuild } from '../../featureEliminationUtil';
-import AllowNotification from '@/components/AllowNotification';
+// import AllowNotification from '@/components/AllowNotification';
 import GetButtonNotification from '@/components/GetButtonNotification';
 import useStore from '@/store/store';
 let SessionCardFooter: ComponentType<any> | null = null;
@@ -920,7 +920,7 @@ const Dashboard: React.FC<DashboardProps> = () => {
       {isClient && (
         <>
           <GuideTour toggleDrawer={toggleDrawer} />
-          <AllowNotification />
+          {/* <AllowNotification /> */}
           <>
             {!isAuthenticated && (
               <Loader showBackdrop={true} loadingText={t('COMMON.LOADING')} />
