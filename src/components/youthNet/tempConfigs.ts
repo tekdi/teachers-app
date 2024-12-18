fe0a5d6e1fd3b8eddf84847b69af07833b29e5bb
Arif-tekdi-technologies <arif.shaikh@tekditechnologies.com>
2024-12-18 17:58:35 +0530

    Issue #PS-2827 feat:  YouthNet Dashboard components/screens implementations (UI only)

---
+++ src/components/youthNet/tempConfigs.ts
@@ -0,0 +1,49 @@
+//** Note : This file will be removed from the project after API's are integrated **//
+
+export const MOCK_SURVEY_CONFIG = {
+  surveyAvailable: true,
+};
+
+export const SURVEY_DATA = {
+  CREATIVITY_MAHOTSAV: 'Creativity Mahotsav',
+  TWELVE: 'Assigned to 12 Villages',
+  FIFTY_TWO: '52',
+};
+
+export const volunteerData = [
+  {
+    id: 1,
+    title: 'Deviyapur',
+    entries: 0,
+    volunteerCount: 0,
+    actionLabel: 'Add or Update Volunteers',
+  },
+  {
+    id: 2,
+    title: 'Kharagpur',
+    entries: 0,
+    volunteerCount: 0,
+    actionLabel: 'Add or Update Volunteers',
+  },
+  {
+    id: 3,
+    title: 'Khopi',
+    entries: 0,
+    volunteerCount: 0,
+    actionLabel: 'Add or Update Volunteers',
+  },
+  {
+    id: 4,
+    title: 'Shivare',
+    entries: 0,
+    volunteerCount: 0,
+    actionLabel: 'Add or Update Volunteers',
+  },
+  {
+    id: 5,
+    title: 'Nandghur',
+    entries: 0,
+    volunteerCount: 0,
+    actionLabel: 'Add or Update Volunteers',
+  },
+];
