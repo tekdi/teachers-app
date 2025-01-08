7703389fc9e9032d1c0a060b253b2bfe9de3152a
Rushikesh-Sonawane99 <rushikesh.sonawane@tekditechnologies.com>
2025-01-08 21:07:35 +0530

    Issue #PS-3392 chore: Updated PR

--- src/pages/feedback.tsx
+++ src/pages/feedback.tsx
@@ -6,45 +6,37 @@ import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const Feedback = () => {
-    const { t } = useTranslation();
-    const queryParams = {
-        username: "JohnDoe",
-        age: 30,
-        isMember: true,
-    };
+  const { t } = useTranslation();
+  const queryParams = {
+    fullName: 'JohnDoe',
+    username: 'JohnD',
+    userid: 'JohnDoe123',
+    email: 'JohnDoe@gmail.com',
+  };
 
-    return (
-        <>
-          <Header />
-          <Box ml={'1rem'}>
-            <Typography
-                mt={4}
-                variant="h2"
-                color= 'black'
-            >
-                {t('COMMON.WE_VALUE_FEEDBACK')}
-            </Typography>
-            <Typography
-                mt={4}
-                variant="h5"
-                marginY={'0.2rem'}
-            >
-                {t('COMMON.SHARE_THOUGHTS')}
-            </Typography>
-            </Box>
-            <JotFormEmbed formId="250065095006449" queryParams={queryParams} />
-
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Box ml={'1rem'}>
+        <Typography mt={4} variant="h2" color="black">
+          {t('COMMON.WE_VALUE_FEEDBACK')}
+        </Typography>
+        <Typography mt={4} variant="h5" marginY={'0.2rem'}>
+          {t('COMMON.SHARE_THOUGHTS')}
+        </Typography>
+      </Box>
+      <JotFormEmbed formId="250065095006449" queryParams={queryParams} />
+    </>
+  );
 };
 
 export async function getStaticProps({ locale }: any) {
-    return {
-        props: {
-            ...(await serverSideTranslations(locale, ['common'])),
-            // Will be passed to the page component as props
-        },
-    };
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+      // Will be passed to the page component as props
+    },
+  };
 }
 
 export default Feedback;
