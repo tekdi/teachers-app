517b9928830704acdb322d2228b4c8a248f66cba
suvarnakale <suvarna.kale@tekditechnologies.com>
2025-01-17 12:32:29 +0530

    Issue #PS-3276 feat: youthnet UI for villages and youth pages

---
+++ src/components/youthNet/SortBy.tsx
@@ -0,0 +1,88 @@
+import { ArrowDropDown } from '@mui/icons-material';
+import { Box, Button, FormControl, Grid } from '@mui/material';
+import React, { useState } from 'react';
+import { useTheme } from '@mui/material/styles';
+import { useTranslation } from 'next-i18next';
+import FilterModalCenter from '@/pages/blocks/components/FilterModalCenter';
+
+const SortBy = () => {
+  const { t } = useTranslation();
+  const theme = useTheme<any>();
+
+  const [filterModalOpen, setFilterModalOpen] = useState(false);
+  const handleFilterModalOpen = () => setFilterModalOpen(true);
+  const handleFilterModalClose = () => setFilterModalOpen(false);
+  const [appliedFilters, setAppliedFilters] = useState({
+    centerType: '',
+    sortOrder: '',
+  });
+  const [centerType, setCenterType] = useState<'regular' | 'remote' | ''>('');
+  const [selectedCenters, setSelectedCenters] = useState<string[]>([]);
+
+  const [sortOrder, setSortOrder] = useState('');
+
+  const handleFilterApply = () => {
+    setAppliedFilters({ centerType, sortOrder });
+    // setFilteredCenters(getFilteredCenters);
+    handleFilterModalClose();
+  };
+  return (
+    <>
+      <Grid item xs={6} mt={'1rem'}>
+        <Box
+          sx={{
+            '@media (min-width: 900px)': {
+              display: 'flex',
+              justifyContent: 'end',
+            },
+          }}
+        >
+          <FormControl
+            className="drawer-select"
+            sx={{
+              width: '100%',
+              '@media (min-width: 900px)': {
+                // width: '40%',
+              },
+            }}
+          >
+            <Button
+              variant="outlined"
+              onClick={() => {
+                handleFilterModalOpen();
+              }}
+              size="medium"
+              endIcon={<ArrowDropDown />}
+              sx={{
+                borderRadius: '7px',
+                border: `1px solid ${theme?.palette?.warning?.A700}`,
+                color: theme?.palette?.warning['300'],
+                pl: 3,
+                fontSize: '13px',
+                fontWeight: '500',
+              }}
+              className="one-line-text"
+            >
+              {t('COMMON.SORT_BY')}
+            </Button>
+          </FormControl>
+        </Box>
+      </Grid>
+
+      <FilterModalCenter
+        open={filterModalOpen}
+        handleClose={handleFilterModalClose}
+        centers={[]}
+        selectedCenters={[]}
+        setSelectedCenters={setSelectedCenters}
+        sortOrder={sortOrder}
+        setSortOrder={setSortOrder}
+        centerType={''}
+        setCenterType={setCenterType}
+        onApply={handleFilterApply}
+      />
+    </>
+  );
+};
+
+export default SortBy;
---
+++ src/pages/youthboard/villages/index.tsx
@@ -0,0 +1,132 @@
+import Header from '@/components/Header';
+import BackHeader from '@/components/youthNet/BackHeader';
+import { Box, Grid, Tab, Tabs, TextField } from '@mui/material';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import React, { useState } from 'react';
+import { useTheme } from '@mui/material/styles';
+import SearchBar from '@/components/Searchbar';
+import SortBy from '@/components/youthNet/SortBy';
+import YouthAndVolunteers from '@/components/youthNet/YouthAndVolunteers';
+import { users, youthList } from '@/components/youthNet/tempConfigs';
+import { UserList } from '@/components/youthNet/UserCard';
+const index = () => {
+  const { t } = useTranslation();
+  const theme = useTheme<any>();
+
+  const [value, setValue] = useState<number>(1);
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box minHeight="100vh">
+      <Box>
+        <Header />
+      </Box>
+      <Box ml={2}>
+        <BackHeader headingOne={t('DASHBOARD.VILLAGES_AND_YOUTH')} />
+      </Box>
+      <Box sx={{ width: '100%' }}>
+        {value && (
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            textColor="inherit" // Use "inherit" to apply custom color
+            aria-label="secondary tabs example"
+            sx={{
+              fontSize: '14px',
+              borderBottom: (theme) => `1px solid #EBE1D4`,
+
+              '& .MuiTab-root': {
+                color: theme.palette.warning['A200'],
+                padding: '0 20px',
+                flexGrow: 1,
+              },
+              '& .Mui-selected': {
+                color: theme.palette.warning['A200'],
+              },
+              '& .MuiTabs-indicator': {
+                display: 'flex',
+                justifyContent: 'center',
+                backgroundColor: theme.palette.primary.main,
+                borderRadius: '100px',
+                height: '3px',
+              },
+              '& .MuiTabs-scroller': {
+                overflowX: 'unset !important',
+              },
+            }}
+          >
+            <Tab value={1} label={t('DASHBOARD.VILLAGES')} />
+            <Tab value={2} label={t('DASHBOARD.YOUTH_VOLUNTEERS')} />
+          </Tabs>
+        )}
+      </Box>
+
+      <Box>
+        {value === 1 && (
+          <>
+            {/* <Grid
+              px={'18px'}
+              spacing={2}
+              mt={1}
+              sx={{ display: 'flex', alignItems: 'center' }}
+              container
+            > */}
+            <Box
+              display={'flex'}
+              flexDirection={'row'}
+              gap={'2rem'}
+              // justifyContent={'space-around'}
+            >
+              <SearchBar
+                onSearch={setSearchInput}
+                value={searchInput}
+                placeholder={t('DASHBOARD.SEARCH_VILLAGES')}
+                fullWidth={false}
+              />
+              <SortBy />
+            </Box>
+            <Box>
+              <YouthAndVolunteers
+                selectOptions={[
+                  { label: 'As of today, 5th Sep', value: 'today' },
+                  { label: 'As of yesterday, 4th Sep', value: 'yesterday' },
+                ]}
+                // data="577 Youth & Volunteers"
+              />
+            </Box>
+            {/* </Grid> */}
+          </>
+        )}
+      </Box>
+      <Box>
+        {value === 2 && (
+          <>
+            {/* <Grid
+              px={'18px'}
+              spacing={2}
+              mt={1}
+              sx={{ display: 'flex', alignItems: 'center' }}
+              container
+            > */}
+            <UserList users={youthList} />
+            {/* </Grid> */}
+          </>
+        )}
+      </Box>
+    </Box>
+  );
+};
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+      // Will be passed to the page component as props
+    },
+  };
+}
+export default index;
