154091ed256673d4b3b2ef6e9406472d94883105
Rushikesh-Sonawane99 <rushikesh.sonawane@tekditechnologies.com>
2025-01-24 10:56:25 +0530

    Issue #PS-3615 chore:Facilitator Assessment View Enhancements

--- src/pages/assessments/index.tsx
+++ src/pages/assessments/index.tsx
@@ -456,6 +456,9 @@ const Assessments = () => {
                 <MenuItem value={'post'} style={{ textAlign: 'right' }}>
                   {t('PROFILE.POST_TEST')}
                 </MenuItem>
+                <MenuItem value={'other'} style={{ textAlign: 'right' }}>
+                  {t('FORM.OTHER')}
+                </MenuItem>
               </Select>
             </FormControl>
           </Box>
@@ -502,7 +505,7 @@ const Assessments = () => {
             >
               {testCompletionCount.totalCount > 0 && (
                 <span>
-                  {`${testCompletionCount.completionCount}/${testCompletionCount.totalCount}`}{' '}
+                  {`${testCompletionCount.completionCount} ${t('ASSESSMENTS.OUT_OF')} ${testCompletionCount.totalCount}`}{' '}
                   {t('ASSESSMENTS.COMPLETED_THE_ASSESSMENT')}
                 </span>
               )}
