e7d1e0718954823ddf88d5bf05558a33561830d9
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2025-01-24 18:30:32 +0530

    Issue feat: Add username validation error message

--- src/components/DynamicForm.tsx
+++ src/components/DynamicForm.tsx
@@ -186,6 +186,16 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
               );
               break;
             }
+            case '^[a-zA-Z0-9.@]+$':
+          
+          {
+            error.message = t(
+              'FORM_ERROR_MESSAGES.SPACE_AND_SPECIAL_CHARACTERS_NOT_ALLOWED'
+            );
+            break;
+
+
+          }
             default: {
               if (error?.property === '.email') {
                 const validEmail = emailPattern.test(pattern);
