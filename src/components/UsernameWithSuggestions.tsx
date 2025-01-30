cfb00fbef6f012e7b6a36b790ff6ba186886c752
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2025-01-30 15:27:36 +0530

    Issue bug : PS-3722 Username suggestions should retain on learner form for usablitiy

--- src/components/UsernameWithSuggestions.tsx
+++ src/components/UsernameWithSuggestions.tsx
@@ -25,6 +25,7 @@ const UsernameWithSuggestions: React.FC<UsernameWidgetProps> = ({
   onChange,
   ...rest
 }) => {
+  console.log("value", value);        
   const { suggestions, onSuggestionSelect } = formContext;
   const { t } = useTranslation();
 
@@ -59,16 +60,15 @@ const UsernameWithSuggestions: React.FC<UsernameWidgetProps> = ({
         <div>
           {suggestions?.map((suggestion: any, index: number) => (
             <Box>
- <Typography variant="h6" color="error" gutterBottom>
-         {t('FORM.USERNAME_ALREADY_EXIST')}
-        </Typography>
-        <Typography variant="h6" color="textSecondary" gutterBottom>
-          {t('FORM.AVAILABLE_SUGGESTIONS')}
-
-        </Typography>             
-         <Typography
+              {value!=="" &&(<Typography variant="h6"  sx={{marginLeft:"12px", mt:"2px"}} color="error" gutterBottom>
+                {t('FORM.USERNAME_ALREADY_EXIST')}
+              </Typography>)}
+              <Typography variant="h6" m="2px"  sx={{marginLeft:"12px"}}color="textSecondary" gutterBottom>
+                {t('FORM.AVAILABLE_SUGGESTIONS')}
+              </Typography>
+              <Typography
                 onClick={() => onSuggestionSelect(suggestion)}
-                sx={{ cursor: 'pointer', color: 'green' }}
+                sx={{ cursor: 'pointer', color: 'green', marginLeft:"12px" }}
               >
                 {suggestion}
               </Typography>
