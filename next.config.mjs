5b15bbfbabbfbdd498a6788ecae06fae562e0a4d
Mahesh Mahajan <mahajanmahesh935@gmail.com>
2025-02-27 11:58:58 +0530

    TASK #0000 : resolve deployment issue

--- next.config.mjs
+++ next.config.mjs
@@ -43,47 +43,47 @@ const nextConfig = {
   },
   async rewrites() {
     return [
-      {
-        source: '/assets/pdfjs/:path*', // Match any URL starting with /workspace/content/assets/
-        destination: `${process.env.WORKSPACE_BASE_URL}/assets/pdfjs/:path*`, // Serve the assets from the public folder
-      },
-      {
-        source: '/play/content/assets/pdfjs/:path*', // Match any URL starting with /workspace/content/assets/
-        destination: `${process.env.WORKSPACE_BASE_URL}/assets/pdfjs/:path*`, // Serve the assets from the public folder
-      },
-      {
-        source: '/play/content/assets/:path*', // Match any URL starting with /workspace/content/assets/
-        destination: `${process.env.WORKSPACE_BASE_URL}/assets/:path*`, // Serve the assets from the public folder
-      },
-      {
-        source: `/action/v1/telemetry`,
-        destination: `${process.env.NEXT_PUBLIC_TELEMETRY_URL}/v1/telemetry`,
-      },
-      {
-        source: '/action/asset/:path*', // Match other /action/asset routes
-        destination: `${process.env.WORKSPACE_BASE_URL}/api/proxy?path=/action/asset/:path*`, // Forward other /action/asset requests to proxy.js
-      },
-      {
-        source: '/action/content/:path*', // Match other /action/asset routes
-        destination: `${process.env.WORKSPACE_BASE_URL}/api/proxy?path=/action/content/:path*`, // Forward other /action/asset requests to proxy.js
-      },
-      {
-        source: '/action/:path*', // Match any other routes starting with /action/
-        destination: `${process.env.WORKSPACE_BASE_URL}/api/proxy?path=/action/:path*`, // Forward them to proxy.js
-      },
-      {
-        source: '/api/:path*', // Match /api/ routes
-        destination: `${process.env.WORKSPACE_BASE_URL}/api/proxy?path=/api/:path*`, // Forward them to proxy.js
-      },
-      {
-        source: '/assets/public/:path*', // Match any URL starting with /assets/public/
-        destination: `${process.env.CLOUD_STORAGE_URL}/:path*`, // Forward to workspace proxy
-      },
+      // {
+      //   source: '/assets/pdfjs/:path*', // Match any URL starting with /workspace/content/assets/
+      //   destination: `${process.env.WORKSPACE_BASE_URL}/assets/pdfjs/:path*`, // Serve the assets from the public folder
+      // },
+      // {
+      //   source: '/play/content/assets/pdfjs/:path*', // Match any URL starting with /workspace/content/assets/
+      //   destination: `${process.env.WORKSPACE_BASE_URL}/assets/pdfjs/:path*`, // Serve the assets from the public folder
+      // },
+      // {
+      //   source: '/play/content/assets/:path*', // Match any URL starting with /workspace/content/assets/
+      //   destination: `${process.env.WORKSPACE_BASE_URL}/assets/:path*`, // Serve the assets from the public folder
+      // },
+      // {
+      //   source: `/action/v1/telemetry`,
+      //   destination: `${process.env.NEXT_PUBLIC_TELEMETRY_URL}/v1/telemetry`,
+      // },
+      // {
+      //   source: '/action/asset/:path*', // Match other /action/asset routes
+      //   destination: `${process.env.WORKSPACE_BASE_URL}/api/proxy?path=/action/asset/:path*`, // Forward other /action/asset requests to proxy.js
+      // },
+      // {
+      //   source: '/action/content/:path*', // Match other /action/asset routes
+      //   destination: `${process.env.WORKSPACE_BASE_URL}/api/proxy?path=/action/content/:path*`, // Forward other /action/asset requests to proxy.js
+      // },
+      // {
+      //   source: '/action/:path*', // Match any other routes starting with /action/
+      //   destination: `${process.env.WORKSPACE_BASE_URL}/api/proxy?path=/action/:path*`, // Forward them to proxy.js
+      // },
+      // {
+      //   source: '/api/:path*', // Match /api/ routes
+      //   destination: `${process.env.WORKSPACE_BASE_URL}/api/proxy?path=/api/:path*`, // Forward them to proxy.js
+      // },
+      // {
+      //   source: '/assets/public/:path*', // Match any URL starting with /assets/public/
+      //   destination: `${process.env.CLOUD_STORAGE_URL}/:path*`, // Forward to workspace proxy
+      // },
 
-      {
-        source: '/app/telemetry', // Match telemetry route
-        destination: `${process.env.WORKSPACE_BASE_URL}/api/telemetry`, // Redirect to telemetry proxy
-      },
+      // {
+      //   source: '/app/telemetry', // Match telemetry route
+      //   destination: `${process.env.WORKSPACE_BASE_URL}/api/telemetry`, // Redirect to telemetry proxy
+      // },
       {
         source: routes.API.GENERAL.CONTENT_PREVIEW,
         destination: `${PORTAL_BASE_URL}${routes.API.GENERAL.CONTENT_PREVIEW}`, // Proxy to portal
@@ -92,10 +92,10 @@ const nextConfig = {
         source: routes.API.GENERAL.CONTENT_PLUGINS,
         destination: `${PORTAL_BASE_URL}${routes.API.GENERAL.CONTENT_PLUGINS}`, // Proxy to portal
       },
-      {
-        source: '/sunbird-plugins/renderer/:path*',
-        destination: `${process.env.WORKSPACE_BASE_URL}/sunbird-plugins/renderer/:path*`,
-      },
+      // {
+      //   source: '/sunbird-plugins/renderer/:path*',
+      //   destination: `${process.env.WORKSPACE_BASE_URL}/sunbird-plugins/renderer/:path*`,
+      // },
     ];
   },
   webpack: (config, { isServer }) => {
