5922da5227a1422784e2aa8d7288b17943c20694
Your Alfaj2302 <alfaj.shaikh@tekditechnologies.com>
2025-03-13 11:26:46 +0530

    Changing Opportunity Tab Icon

--- src/components/MenuDrawer.tsx
+++ src/components/MenuDrawer.tsx
@@ -448,19 +448,19 @@ const MenuDrawer: React.FC<DrawerProps> = ({
                     : 'transparent',
                 },
               }}
-              startIcon={<Image
-                src={surveyForm}
-                alt="SurveyForm-Icon"
-                width={24}
-                height={24}
-              />}
+              startIcon={
+                <Image
+                  src={surveyForm}
+                  alt="SurveyForm-Icon"
+                  width={24}
+                  height={24}
+                />
+              }
               onClick={() => {
                 router.push(`/youthboard/surveys`);
               }}
             >
-              {
-                t('SURVEYS.SURVEYS')
-              }
+              {t('SURVEYS.SURVEYS')}
             </Button>
           </Box>
         )}
@@ -514,7 +514,7 @@ const MenuDrawer: React.FC<DrawerProps> = ({
                   ? theme.palette.primary.main
                   : 'transparent',
 
-                padding:'16px 18px !important',
+                padding: '16px 18px !important',
                 color: isOpportunity ? '#2E1500' : theme.palette.warning.A200,
                 fontWeight: isOpportunity ? '600' : 500,
                 '&:hover': {
@@ -526,9 +526,7 @@ const MenuDrawer: React.FC<DrawerProps> = ({
                 gap: '10px',
               }}
               startIcon={
-                <LocalLibraryOutlinedIcon
-                  sx={{ fontSize: '24px !important' }}
-                />
+                <DashboardOutlinedIcon sx={{ fontSize: '24px !important' }} />
               }
               onClick={() => {
                 router.push(`/opportunities`);
